77a0e00812b1b6d2462ede30a60dfea26c001a3c
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-25 17:24:21 -0300

    Project structure

---
+++ src/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
+const express = require("express");
+
+class App {
+  constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(require("./routes"));
+  }
+}
+
+module.exports = new App().express;

c33f370d59c3cdf47ea6c14c8830da3db778573a
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-26 02:09:40 -0300

    User token - Unit test

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const { User } = require("../../src/app/models");
 const truncate = require("../utils/truncate");
@@ -18,5 +20,19 @@ describe("User", () => {
     const compareHash = await bcrypt.compare(user.password, user.password_hash);
 
     expect(compareHash).toBe(true);
+  });
+
+  it("should generate a valid token", async () => {
+    const user = await User.create({
+      name: "test",
+      email: "test@test.com",
+      password: "123123"
+    });
+
+    const token = user.generateToken();
+
+    const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
+
+    expect(decoded.id).toBe(user.id);
   });
 });

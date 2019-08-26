3b2aba4456216244f83e8ccd9de27bd263900958
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-26 12:40:00 -0300

    Access private routes when not authenticated - Code

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,12 @@ const routes = express.Router();
 const SessionController = require("../src/app/controllers/SessionController");
 const DashboardController = require("../src/app/controllers/DashboardController");
 
+const authMiddleware = require("../src/app/middlewares/auth");
+
 routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.get("/dashboard", DashboardController.index);
 
 module.exports = routes;

9e87cb0b3e7d24f32010fd31c1d26fe690ff92c1
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-26 02:43:59 -0300

    Session create with invalid credentials - Integration test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -24,4 +24,19 @@ describe("Authentication", () => {
 
     expect(response.status).toBe(200);
   });
+
+  it("should not be able to authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "testnotauthenticate",
+      email: "testnotauthenticate@test.com",
+      password: "123123"
+    });
+
+    const response = await request.post("/sessions").send({
+      email: user.email,
+      password: "123456"
+    });
+
+    expect(response.status).toBe(401);
+  });
 });

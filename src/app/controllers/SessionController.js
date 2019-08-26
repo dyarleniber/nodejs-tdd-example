aac5b9934f7f397c580fb13230fe0b73a2162aad
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-26 02:38:03 -0300

    Session create - Code refactor

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,18 @@
+const { User } = require("../models");
+
 class SessionController {
   async store(req, res) {
+    const { email } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
     return res.json({
-      message: "ok"
+      user,
+      token: user.generateToken()
     });
   }
 }

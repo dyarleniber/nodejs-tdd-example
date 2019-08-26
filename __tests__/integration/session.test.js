b0dcfa30abb460fe2c07381c1aa736a8b9b233d6
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-26 02:39:35 -0300

    Session create - Code refactor

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,22 +1,26 @@
-const request = require("supertest");
-
+const supertest = require("supertest");
 const app = require("../../src/app");
+const request = supertest(app);
+
+const truncate = require("../utils/truncate");
 const { User } = require("../../src/app/models");
 
 describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it("should be able to authenticate with valid credentials", async () => {
     const user = await User.create({
-      name: "test",
-      email: "test@test.com",
+      name: "testauthenticate",
+      email: "testauthenticate@test.com",
       password: "123123"
     });
 
-    const response = await request(app)
-      .post("/sessions")
-      .send({
-        email: user.email,
-        password: user.password
-      });
+    const response = await request.post("/sessions").send({
+      email: user.email,
+      password: user.password
+    });
 
     expect(response.status).toBe(200);
   });
